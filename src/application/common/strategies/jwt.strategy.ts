5ede7d48b2882eb67ba497988f61c98059c8e9ee
minhduc <minhductxqt@gmail.com>
2024-06-01 21:16:58 +0700

    fix: get JWT secret from environment config service

--- src/application/common/strategies/jwt.strategy.ts
+++ src/application/common/strategies/jwt.strategy.ts
@@ -4,15 +4,19 @@ import { PassportStrategy } from '@nestjs/passport';
 import { User } from '@app/domain/todo/entities/user';
 import { IJwtServicePayload } from '../adapters/jwt.interface';
 import { AuthService } from '@app/application/todo/use-cases/auth/auth.service';
+import { EnvironmentConfigService } from '@app/infrastructure/config/environment-config';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor(private readonly authService: AuthService) {
+  constructor(
+    private readonly authService: AuthService,
+    environmentConfigService: EnvironmentConfigService,
+  ) {
     super({
       usernameField: 'email',
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: process.env.JWT_SECRET,
+      secretOrKey: environmentConfigService.get('JWT_SECRET'),
     });
   }
 
